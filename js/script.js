2eebfc0a249ad244f4af8ea0d698b4358cb451ec
Antoine Lelievre <alelievre@fabriquenumerique.fr>
2021-03-21 19:20:15 +0100

    contenu v1 sans formes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-let burger = document.getElementById('burger');
-let menu = document.getElementById('menu');
+let body = document.getElementById('body')
+let logoname = document.getElementById('name')
+let circle = document.getElementById('logo-circle')
+let entete = document.getElementById('entete')
+let header = document.getElementsByTagName("header")[0]
 
-burger.onclick = function(){
-    menu.classList.toggle('hidden')
+body.onscroll = function(){
+    if(window.scrollY == "0"){
+        logoname.classList.remove('name_small')
+        circle.classList.remove('circle_small')
+        entete.classList.remove('entete_small')
+        header.classList.remove('header_small')
+    }
+    else{
+        logoname.classList.add('name_small')
+        circle.classList.add('circle_small')
+        entete.classList.add('entete_small')
+        header.classList.add('header_small')
+        header.style='scale(0.5)'
+    }
 }
