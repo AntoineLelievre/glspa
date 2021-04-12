3140ccdd7b7a28cc69f98d21a30c5e946e07765c
Antoine Lelievre <alelievre@fabriquenumerique.fr>
2021-04-12 19:49:20 +0200

    v1 full pc

--- js/script.js
+++ js/script.js
@@ -3,19 +3,29 @@ let logoname = document.getElementById('name')
 let circle = document.getElementById('logo-circle')
 let entete = document.getElementById('entete')
 let header = document.getElementsByTagName("header")[0]
-
+let back = document.getElementById('back1')
+let titre = document.getElementById('backtitre')
+let num = document.getElementById('num')
 body.onscroll = function(){
+   
     if(window.scrollY == "0"){
         logoname.classList.remove('name_small')
         circle.classList.remove('circle_small')
         entete.classList.remove('entete_small')
         header.classList.remove('header_small')
+        num.classList.remove('num_small')
+        titre.style.marginTop="140px"
     }
     else{
         logoname.classList.add('name_small')
         circle.classList.add('circle_small')
         entete.classList.add('entete_small')
         header.classList.add('header_small')
+        num.classList.add('num_small')
+
         header.style='scale(0.5)'
+
+        /* console.log(((back.scrollHeight-window.scrollY)-header.scrollHeight)/2)
+        titre.style.marginTop=(((back.scrollHeight-window.scrollY)-header.scrollHeight))+"px" */
     }
 }
